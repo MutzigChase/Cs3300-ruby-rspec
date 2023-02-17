02f1940f4ba66045e3c8dc5f70618dec76929c98
Chase Mutzig <mutzigchase@gmail.com>
2023-02-17 00:54:03 +0000

    finished code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,77 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  #uses built in sum method. Shown in class.
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  #returns zero if no elements
+  return 0 if arr.empty?
+  #if only one element, return it
+  if arr.length == 1 then return arr.first end
+
+  #makes array only the largest numbers
+  arr = arr.max(2)
+  #returns the sum of the largest numbers
+  arr.sum
+
+
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+    #returns false if no elements
+    return false if arr.empty?
+    #if only one element, return flase
+    if arr.length == 1 then return false end
+    
+      #handy method found to check specified permutations. 
+      #Checks if any two elements add to given n
+    arr.permutation(2).any? {|a,b| a + b == n}
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  #says hello using the variable name
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  #converts all letters to upper case and compares start with consonants
+  s.upcase.start_with?('B', 'C', 'D', 'F', 'G', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'S', 'T', 'V', 'X', 'Z' 'H', 'R', 'W', 'Y')
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s == "0"
+  /^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+#makes the isbn attribute
+attr_accessor :isbn
+#makes the price attribute
+attr_accessor :price
+
+#constructor
+def initialize(isbn, price)
+  #If the isbn is invalid, reject it
+  if isbn.empty? then raise ArgumentError end
+  #if the price is invalid, reject it
+  if price <= 0 then raise ArgumentError end
+#sets isbn
+@isbn = isbn
+#sets price
+@price = price
+end #end constructor
+
+#shows the price as a string
+def price_as_string
+  #returns $price with 2 decimal places
+  return "$%0.2f" % [price]
+end
+
 end
--- spec/part1_spec.rb
+++ spec/part1_spec.rb
@@ -5,15 +5,22 @@ require_relative '../lib/ruby_intro'
 describe 'Ruby intro part 1' do
   describe '#sum' do
 
+    #This explains what the test is and how many points it is worth
     it 'returns correct sum [1 point]', points: 1 do
+      #Expects the sum of array elements to be an integer
       expect(sum([1, 2, 3, 4, 5])).to be_a_kind_of Integer
+      #Expects the sum of array elements to be 15
       expect(sum([1, 2, 3, 4, 5])).to eq(15)
+      #Expects the sum of array elements to be 5
       expect(sum([1, 2, 3, 4, -5])).to eq(5)
+      #Expects the sum of the array elements to be -90
       expect(sum([1, 2, 3, 4, -5, 5, -100])).to eq(-90)
     end
 
     it 'works on the empty array [2 points]', points: 2 do
+      #Expects sum command to accept nothing
       expect { sum([]) }.not_to raise_error
+      #Expects sum command to be zero when given nothing
       expect(sum([])).to be_zero
     end
   end
@@ -21,6 +28,7 @@ describe 'Ruby intro part 1' do
   describe '#max_2_sum' do
     
     it 'returns the correct sum [1 point]', points: 1 do
+      max_2_sum([1, 2, 3, 4, 5])
       expect(max_2_sum([1, 2, 3, 4, 5])).to be_a_kind_of Integer
       expect(max_2_sum([1, -2, -3, -4, -5])).to eq(-1)
     end
